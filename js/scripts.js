5663cfb30b869477332238c4e12fe73908b1a0d5
Sudheer <Sudheer.babu@gmail.com>
2020-02-10 12:10:46 +0530

    second mail code change

--- js/scripts.js
+++ js/scripts.js
@@ -251,29 +251,29 @@
 	// 	});
 	// }
 
-	// sendMailForm
-	$('#sendMailForm').submit(function (e) {
+	$( '#sendMailForm' ).submit(function ( e ) {
 		var data = {
 			'name': $('#name').val(),
 			'email': $('#email').val(),
-			'subject': $('#subject').val(),
-			'message': $('#message').val()
+			'contact': $('#contactNumber').val(),
+			'message' : $('#message').val()
 		};
-
-		$.ajax({
-			url: 'mail.php',
+		// POST data to the php file
+		$.ajax({ 
+			url: 'mail.php', 
 			data: data,
 			type: 'POST',
 			success: function (data) {
+				// For Notification
 				document.getElementById("sendMailForm").reset();
 				var $alertDiv = $(".mailResponse");
 				$alertDiv.show();
 				$alertDiv.find('.alert').removeClass('alert-danger alert-success');
 				$alertDiv.find('.mailResponseText').text("");
-				if(data.error) {
-					$alertDiv.find(".alert").addClass('alert-danger');
+				if(data.error){
+					$alertDiv.find('.alert').addClass('alert-danger');
 					$alertDiv.find('.mailResponseText').text(data.message);
-				} else {
+				}else{
 					$alertDiv.find('.alert').addClass('alert-success');
 					$alertDiv.find('.mailResponseText').text(data.message);
 				}
